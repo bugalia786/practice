f80149632a8ea82560708f206811dcb8c4a3e604
bugalia786 <58610957+bugalia786@users.noreply.github.com>
2020-06-09 07:27:30 +0000

    Add files via upload

--- iterativemergesort.cpp
+++ iterativemergesort.cpp
@@ -1,6 +1,12 @@
 //iterative Merge sort
 #include <iostream>
 using namespace std;
+void display(int *A,int n){
+    for(int i=0;i<n;i++){
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
+}
 void MergeSingle(int *a,int low,int mid,int high){
     int i=low,j=mid+1,k=low,b[high+1];
     while(i<=mid && j<=high){
@@ -16,37 +22,36 @@ void MergeSingle(int *a,int low,int mid,int high){
     }
 }
 void iterative_merge_sort(int *A,int n){
-    int p,l,h,mid;
+    int p,l,h,mid,i;
     for(p=2;p<=n;p=p*2){
-        for(int i=0;i+p-1<n;i=i+p){
+        for(i=0;i<n;i=i+p){
             l=i;
             h=i+p-1;
+            if(h>=n){
+                h=n-1;
+            }
             mid=(l+h)/2;
             MergeSingle(A,l,mid,h);
         }
+        //display(A,7);
     }
     if(p/2<n){
-        MergeSingle(A,0,p/2-1,n);
+        MergeSingle(A,0,p/2-1,n-1);
     }
 }
-void display(int *A,int n){
-    for(int i=0;i<n;i++){
-        cout<<A[i]<<" ";
-    }
-    cout<<endl;
-}
+
 int main() {
-	int A[]={1,17,5,15,7,4,3};//output 1 4 5 7 3 1 15   Why is it so?.......
-	int a[]={1,17,5,15,7,4,3,6};//output 1 3 4 5 6 7 15 17 
-	int B[]={3,7,4,1,5,9};//output 1 3 4 5 7 9
-	int D[7]={1,4,6,2,3,7,8};//output 1 2 3 4 6 7 8
+	int A[]={1,17,5,15,7,4,3};
+ 	int a[]={1,17,5,15,7,4,3,6};
+ 	int B[]={3,7,4,1,5,9};
+ 	int D[7]={1,4,6,2,3,7,8};
 	iterative_merge_sort(A,7);
 	display(A,7);
-	iterative_merge_sort(a,8);
-	display(a,8);
-	iterative_merge_sort(B,6);
-	display(B,6);
-	iterative_merge_sort(D,7);
-	display(D,7);
+ 	iterative_merge_sort(a,8);
+ 	display(a,8);
+ 	iterative_merge_sort(B,6);
+ 	display(B,6);
+ 	iterative_merge_sort(D,7);
+ 	display(D,7);
 	return 0;
 }
